75743fd5712ed8c4c0253430babcd943ed5310b6
kim <iufan1526@gmail.com>
2024-01-26 14:39:39 +0900

    update : 팀 상세화면 업데이트

--- src/pages/TeamDetail/index.tsx
+++ src/pages/TeamDetail/index.tsx
@@ -3,26 +3,11 @@ import TitleText from 'components/text/TitleText';
 import Layout from 'layouts/App';
 import './team-detail.css';
 import DlText from 'components/text/DlText';
-import { Card, Dropdown } from 'react-bootstrap';
+import { Button, Card, CardGroup, Dropdown } from 'react-bootstrap';
 import styled from 'styled-components';
+// import MyResponsivePie from 'components/graph/MyResponsePie';
 
 const TeamDetail = () => {
-    const TripleContainer = styled.div`
-        display: flex;
-        width: 80%; // 부모의 전체 너비를 차지하도록 설정
-        height: 95vh; // 높이를 화면의 전체 높이로 설정할 수도 있습니다
-        // border: 2px solid #b3d4fc; // 옅은 푸른색 테두리 적용
-        // border-radius: 15px; // 모서리 둥글게 설정
-
-        > div {
-            flex: 1; // 세 개의 div가 부모의 공간을 균등하게 나누어 가짐
-            //border: 2px solid green; // 각 div의 테두리를 초록색으로 설정
-            &:not(:last-child) {
-                margin-right: 2px; // 오른쪽 div에만 여백을 추가하여 구분
-            }
-        }
-    `;
-
     const stats = {
         played: 32,
         won: 24,
@@ -35,47 +20,122 @@ const TeamDetail = () => {
         rank: '8위',
     };
 
+    const ScoreboardContainer = styled.div`
+        width: 100%;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        background: white;
+        border-radius: 8px;
+        padding: 20px;
+        box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+        max-height: 90vh; /* Maximum height of the scoreboard container */
+        overflow-y: auto; /* Enable vertical scrolling */
+        :: -webkit-scrollbar {
+            /* Hide scrollbar for Chrome, Safari and Opera */
+            display: none;
+        }
+    `;
+
     return (
         <Layout>
-            <Card>
-                <div className="team-info-preview">
-                    <ImageView />
-                    <div className="team-div">
-                        <TitleText title="맨체스터 유나이티드1" />
-                        <div className="">
-                            <DlText title="감독" content="김승태" />
-                            <DlText title="연고지" content="경기도 수원시 권선구" />
-                        </div>
-                        <div>
-                            <div className="team-div">
-                                <h5>2023</h5>
-                                <Dropdown>
-                                    <Dropdown.Toggle variant="success" id="dropdown-basic">
-                                        년도
-                                    </Dropdown.Toggle>
-                                    <Dropdown.Menu>
-                                        <Dropdown.Item>2022</Dropdown.Item>
-                                        <Dropdown.Item>2021</Dropdown.Item>
-                                    </Dropdown.Menu>
-                                </Dropdown>
+            <ScoreboardContainer>
+                <Card>
+                    <div className="team-info-preview">
+                        <ImageView />
+                        <div className="team-div">
+                            <TitleText title="맨체스터 유나이티드1" />
+                            <div className="">
+                                <DlText title="감독" content="김승태" />
+                                <DlText title="연고지" content="경기도 수원시 권선구" />
                             </div>
-                            <div className="team-div">
-                                <DlText title="승" content="2" className="team-dl" />
-                                <DlText title="무" content="2" className="team-dl" />
-                                <DlText title="패" content="2" className="team-dl" />
-                            </div>
-                            <div className="team-div">
-                                <DlText title="득점" content="2" className="team-dl" />
-                                <DlText title="실점" content="2" className="team-dl" />
-                                <DlText title="경기" content="2" className="team-dl" />
+                            <div>
+                                <div className="team-div">
+                                    <h5>2023</h5>
+                                    <Dropdown>
+                                        <Dropdown.Toggle variant="success" id="dropdown-basic">
+                                            년도
+                                        </Dropdown.Toggle>
+                                        <Dropdown.Menu>
+                                            <Dropdown.Item>2022</Dropdown.Item>
+                                            <Dropdown.Item>2021</Dropdown.Item>
+                                        </Dropdown.Menu>
+                                    </Dropdown>
+                                </div>
+                                <div className="team-div">
+                                    <DlText title="승" content="2" className="team-dl" />
+                                    <DlText title="무" content="2" className="team-dl" />
+                                    <DlText title="패" content="2" className="team-dl" />
+                                </div>
+                                <div className="team-div">
+                                    <DlText title="득점" content="2" className="team-dl" />
+                                    <DlText title="실점" content="2" className="team-dl" />
+                                    <DlText title="경기" content="2" className="team-dl" />
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-            </Card>
-            <Card>
-                <div>시즌기록</div>
-            </Card>
+                </Card>
+                <Card>
+                    <TitleText title="통계" />
+                    {/* <MyResponsivePie /> */}
+                </Card>
+                <Card>
+                    <TitleText title="탑 플레이어" />
+                    <CardGroup>
+                        <Card className="team-card">
+                            <p>득점</p>
+                            <p>1</p>
+                            <Card.Img
+                                variant="top"
+                                src="https://img1.daumcdn.net/thumb/S200x200/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fsports%2Fplayer%2F300%2F14%2F908372.jpg&scode=default_face_profile_big_p"
+                            />
+                            <Card.Body>
+                                <Card.Text>2. 래시포드</Card.Text>
+                                <Card.Text>3. 페르난데스</Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="team-card">
+                            <p>도움</p>
+                            <p>1</p>
+                            <Card.Img
+                                variant="top"
+                                src="https://img1.daumcdn.net/thumb/S200x200/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fsports%2Fplayer%2F300%2F14%2F288205.jpg&scode=default_face_profile_big_p"
+                            />
+                            <Card.Body>
+                                <Card.Text>2. 래시포드</Card.Text>
+                                <Card.Text>3. 페르난데스</Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="team-card">
+                            <p>공격P</p>
+                            <p>1</p>
+                            <Card.Img
+                                variant="top"
+                                src="https://img1.daumcdn.net/thumb/S76x76/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fsports%2Fplayer%2F300%2F14%2F903830.jpg&scode=default_face_profile_big_p"
+                            />
+                            <Card.Body>
+                                <Card.Text>2. 래시포드</Card.Text>
+                                <Card.Text>3. 페르난데스</Card.Text>
+                            </Card.Body>
+                        </Card>
+                        <Card className="team-card">
+                            <p>출전수</p>
+                            <p>1</p>
+                            <Card.Img
+                                variant="top"
+                                src="https://img1.daumcdn.net/thumb/S76x76/?fname=https%3A%2F%2Ft1.daumcdn.net%2Fsports%2Fplayer%2F300%2F14%2F772389.jpg&scode=default_face_profile_big_p"
+                            />
+                            <Card.Body>
+                                <Card.Text>
+                                    <Card.Text>2. 래시포드</Card.Text>
+                                    <Card.Text>3. 페르난데스</Card.Text>
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </CardGroup>
+                </Card>
+            </ScoreboardContainer>
         </Layout>
     );
 };
--- src/pages/playerStat/index.tsx
+++ src/pages/playerStat/index.tsx
@@ -3,13 +3,15 @@ import { Bar } from 'react-chartjs-2';
 import './playerstat.css';
 
 const dataForTotalGamesPlayed = {
-  labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-  datasets: [{
-    data: [2, 3, 1, 3, 2, 3, 1],
-    backgroundColor: 'rgba(255, 99, 132, 0.2)',
-    borderColor: 'rgba(255, 99, 132, 1)',
-    borderWidth: 1
-  }]
+    labels: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+    datasets: [
+        {
+            data: [2, 3, 1, 3, 2, 3, 1],
+            backgroundColor: 'rgba(255, 99, 132, 0.2)',
+            borderColor: 'rgba(255, 99, 132, 1)',
+            borderWidth: 1,
+        },
+    ],
 };
 // const optionsForTotalGamesPlayed = {
 //     scales: {
@@ -22,30 +24,28 @@ const dataForTotalGamesPlayed = {
 //       },
 //     },
 //   } as const;
-  
-  
 
 const PlayerStatistics = () => {
-  return (
-    <div>
-      <div className="card">
-        <div className="card-header">Total Players</div>
-        <div className="card-body">22</div>
-        <button>Add Player</button>
-      </div>
+    return (
+        <div>
+            <div className="card">
+                <div className="card-header">Total Players</div>
+                <div className="card-body">22</div>
+                <button>Add Player</button>
+            </div>
 
-      <div className="card">
-        <div className="card-header">Team Size</div>
-        <div className="card-body">14</div>
-        <button>Add Members</button>
-      </div>
+            <div className="card">
+                <div className="card-header">Team Size</div>
+                <div className="card-body">14</div>
+                <button>Add Members</button>
+            </div>
 
-      <div className="card">
-        <div className="card-header">Total Games Played</div>
-        {/* <Bar data={dataForTotalGamesPlayed} options={optionsForTotalGamesPlayed as any} /> */}
-      </div>
-    </div>
-  );
+            <div className="card">
+                <div className="card-header">Total Games Played</div>
+                {/* <Bar data={dataForTotalGamesPlayed} options={optionsForTotalGamesPlayed as any} /> */}
+            </div>
+        </div>
+    );
 };
 
 export default PlayerStatistics;
