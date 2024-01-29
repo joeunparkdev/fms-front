21c9a7df77c54caf9e3700788279fb6f72a89123
kim <iufan1526@gmail.com>
2024-01-29 11:57:50 +0900

    feat : 팀 상세조회 화면

---
+++ src/components/graph/MyResponsiveRadar.tsx
@@ -0,0 +1,53 @@
+import { ResponsiveRadar } from '@nivo/radar';
+
+export interface MyResponsiveRadarType {
+    data: Array<{
+        stats: string;
+        myTeam: number;
+        avgTeam: number;
+    }>;
+}
+
+const MyResponsiveRadar = ({ data }: any) => {
+    return (
+        <ResponsiveRadar
+            data={data}
+            keys={['myTeam', 'avgTeam']}
+            indexBy="stats"
+            maxValue={100}
+            // valueFormat=">-.2f"
+            margin={{ top: 70, right: 80, bottom: 40, left: 80 }}
+            borderColor={{ from: 'color' }}
+            gridLabelOffset={36}
+            dotSize={10}
+            dotColor={{ theme: 'background' }}
+            dotBorderWidth={2}
+            colors={{ scheme: 'nivo' }}
+            blendMode="multiply"
+            motionConfig="wobbly"
+            legends={[
+                {
+                    anchor: 'top-left',
+                    direction: 'column',
+                    translateX: -50,
+                    translateY: -40,
+                    itemWidth: 80,
+                    itemHeight: 20,
+                    itemTextColor: '#999',
+                    symbolSize: 12,
+                    symbolShape: 'circle',
+                    effects: [
+                        {
+                            on: 'hover',
+                            style: {
+                                itemTextColor: '#000',
+                            },
+                        },
+                    ],
+                },
+            ]}
+        />
+    );
+};
+
+export default MyResponsiveRadar;
--- src/components/image/ImageView.tsx
+++ src/components/image/ImageView.tsx
@@ -2,13 +2,11 @@ import './image-view.css';
 
 const ImageView = (props: any) => {
     return (
-        <div className="team-info-preview-image">
-            <img
-                src={`https://i.namu.wiki/i/dRKhyZ5tYqqxc9FBvHlE-R2u2cW_C6SF62QKaKo-9g2JjMMtvIRJYGZvWv5oVP8N0kiWSz70bcjErVapkIDpl7NfNEay1JIdRf6MVpE-YRXFRW5hEpYlfg7CgrO-IThd6Ti93qnjnHUvYT2vA9Sd0g.svg`}
-                alt=""
-                className="team-iamge"
-            />
-        </div>
+        <img
+            src={`https://i.namu.wiki/i/dRKhyZ5tYqqxc9FBvHlE-R2u2cW_C6SF62QKaKo-9g2JjMMtvIRJYGZvWv5oVP8N0kiWSz70bcjErVapkIDpl7NfNEay1JIdRf6MVpE-YRXFRW5hEpYlfg7CgrO-IThd6Ti93qnjnHUvYT2vA9Sd0g.svg`}
+            alt=""
+            className="team-iamge"
+        />
     );
 };
 
