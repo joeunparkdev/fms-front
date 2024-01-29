c1478bb50e1d792ba07d2ee4ba5683d0f4ed261d
HoyeongJeon <hoyeong0302@gmail.com>
2024-01-29 17:06:26 +0900

    feat: 팀 경기 결과 및 데이터 전송

--- src/store/teamStore.ts
+++ src/store/teamStore.ts
@@ -3,17 +3,20 @@ import { create } from "zustand";
 interface TeamStore {
   teamId?: number;
   name?: string;
-  setTeamInfo: (teamId: number, name: string) => void;
+  imageUUID?: string;
+  setTeamInfo: (teamId: number, name: string, imageUUID: string) => void;
   setTeamId: (teamId: number) => void;
 }
 
 export const useTeamStore = create<TeamStore>((set) => ({
   teamId: undefined,
   name: "",
-  setTeamInfo: (teamId: number, name: string) =>
+  imageUUID: "",
+  setTeamInfo: (teamId: number, name: string, imageUUID: string) =>
     set({
       teamId,
       name,
+      imageUUID,
     }),
 
   setTeamId: (teamId: number) =>
